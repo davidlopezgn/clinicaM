6d899f88b4a8e974997553432cc1d0170eef85b6
Josue <j.pll@hotmail.com>
2020-10-12 18:21:16 -0500

    Consultorio

---
+++ src/main/java/com/clinica/jpaclinica/jpa/Consultorio.java
@@ -0,0 +1,48 @@
+package com.clinica.jpaclinica.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Consultorio {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ConsultorioId")
+	private Long id;
+	@Column(name="Ubicacion")
+	private String ubicacion;
+	@Column(name="Estado")
+	private String estado;
+	@Column(name="Tipo")
+	private String tipo;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	@Override
+	public String toString() {
+		return "Consultorio [getId()=" + this.getId() + ", getUbicacion()=" + this.getUbicacion() + ", getEstado()=" + this.getEstado()
+				+ ", getTipo()=" + this.getTipo() +  "]";
+	}
+}
