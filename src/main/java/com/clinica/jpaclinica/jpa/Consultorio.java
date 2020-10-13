ff7e6a8ee04ee20f183cb1d767e034039c364402
Josue <j.pll@hotmail.com>
2020-10-12 19:29:41 -0500

    entity

--- src/main/java/com/clinica/jpaclinica/jpa/Consultorio.java
+++ src/main/java/com/clinica/jpaclinica/jpa/Consultorio.java
@@ -1,10 +1,11 @@
 package com.clinica.jpaclinica.jpa;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class Consultorio {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
